110bba6a9df6ed15c78c2b3c49c8e0c0cda40fda
Michael Holtz <mjh9122@rit.edu>
2023-06-10 22:09:19 -0400

    Fasta.py docstring

--- Solutions/Fasta.py
+++ Solutions/Fasta.py
@@ -10,20 +10,38 @@ from tqdm import tqdm
 
 
 class Fasta():
-    # Lines should be strait from a txt file, unaltered
     def __init__(self, lines):
+        """Initialize a Fasta object, consisting of a label, and a string (DNA, RNA, Amino Acids)
+
+        Args:
+            lines (List): First line is the label, starting with a >. The following lines are the string of the object
+        """
         assert lines[0][0] == '>'
         self.id = lines[0][1:].strip()
         self.string = ""
         for line in lines[1:]:
             self.string += line.strip()
     
-    # String representation of a Fasta object
     def __repr__(self) -> str:
+        """String representation, label and string
+
+        Returns:
+            str: see above
+        """
         return self.id + " " + self.string
     
-    # The number of differing characters when a comparison string is aligned with the string of the Fasta object
     def hamming_dist(self, comparison_string: str) -> int:
+        """Calculate the hamming distance to a given string
+
+        Args:
+            comparison_string (str): The string to compare to
+
+        Raises:
+            Exception: Raised when the comparison string and self.string do not have the same length
+
+        Returns:
+            int: The number of characters that to not match when both strings are aligned by index
+        """
         if len(self.string) != len(comparison_string):
             raise Exception("DNA lengths must be the same for hamming distance")
         else:
@@ -33,8 +51,15 @@ class Fasta():
                     diff += 1
         return diff
     
-    # Start locations of a motif (substring) with in a Fasta object's string
     def motif_locations(self, substring) -> list:
+        """Finds the index of each appearance of the given substring
+
+        Args:
+            substring (str): substring to locate 
+
+        Returns:
+            list: list of indices where the substring appears
+        """
         locations = []
         length = len(substring)
         for index in range(len(self.string)):
@@ -42,8 +67,15 @@ class Fasta():
                 locations.append(index + 1)
         return locations
     
-   # Indices of a sequence found within a Fasta object, not necessarily contiguous
-    def subsequence_indices(self, sequence_ob):
+    def subsequence_indices(self, sequence_ob)->list:
+        """Finds the indices of the subsequence in the Fasta object's string
+
+        Args:
+            sequence_ob (Fasta): The object whose string will be looked for as a subsequence
+
+        Returns:
+            list: list if indices that form the subsequence, if not all are found, returns list of as many as exist in the Fasta string
+        """
         indices = []
         cur = 0
         for i, char in enumerate(self.string):
@@ -54,9 +86,16 @@ class Fasta():
             except:
                 pass
         return indices
-    
-    # Returns the length of the longest common subsequence of this and one other Fasta_object
-    def common_subsequence(self, comparison_ob):
+
+    def common_subsequence(self, comparison_ob)->str:
+        """Finds the longest common subsequence with another Fasta object
+
+        Args:
+            comparison_ob (Fasta): Fasta object whose string will be used to find the longest common subsequence
+
+        Returns:
+            str: the longest common subsequence (only one even if multiple are possible)
+        """
         rows = self.string
         cols = comparison_ob.string
         longest_substr = [["" for _ in range(len(cols)+1)] for _ in range(len(rows)+1)]
@@ -70,9 +109,17 @@ class Fasta():
                     longest_substr[r][c] = longest_substr[r-1][c]
         return longest_substr[-1][-1]
     
-    # Returns the minimum edit distance between this and one other Fasta Object
-    # In this count, insertions, deletions, and swaps all count as 1 
+
     def edit_distance(self, comparison_ob):
+        """Finds the minimum edit distance between this and one other Fasta Object. 
+        In this count, insertions, deletions, and swaps all count as 1 
+
+        Args:
+            comparison_ob (Fasta): Object whose string will be compared to
+
+        Returns:
+            int : number of operations needed to change Fasta string into comparison object's string
+        """
         rows = self.string
         cols = comparison_ob.string
         dist = [[0 for _ in range(len(cols)+1)] for _ in range(len(rows)+1)]
@@ -88,11 +135,19 @@ class Fasta():
                     dist[r][c] = min(dist[r-1][c], dist[r-1][c-1], dist[r][c-1]) + 1
         return dist[-1][-1]
     
-    # Returns the minimum edit distance and alignment of two strings
-    # Ex. 
-    # in: PRETTY PRTTEIN
-    # out: (4, PRETTY--, PR-TTEIN)
-    def edit_dist_allignment(self, comparison_ob):
+    
+    def edit_dist_alignment(self, comparison_ob):
+        """Returns the minimum edit distance and alignment of two strings
+        Ex. 
+        in: PRETTY PRTTEIN
+        out: (4, PRETTY--, PR-TTEIN)
+
+        Args:
+            comparison_ob (Fasta): Object whose string will be compared to
+
+        Returns:
+            tuple: (edit distance, self.string with - included, comparison string with - included)
+        """
         rows = self.string
         cols = comparison_ob.string
         # (ROW, COL, NUM)
@@ -120,8 +175,18 @@ class Fasta():
         return dist[-1][-1]
     
 
-    # Returns the optimal local allignment of two protein strings, as defined by the mapping passed into the function
+   
     def optimal_local_alignment(self, comparison_ob, mapper, gap_pen):
+        """Finds the optimal local alignment of two strings, as defined by the mapping passed into the function
+
+        Args:
+            comparison_ob (Fasta): Object whose string will be compared to
+            mapper (dict): dict whose keys should be the substitution chars, and values should be the score associated with them
+            gap_pen (int): the penalty for insertions or deletions
+
+        Returns:
+            tuple: (max score, self.string substring associated with max score, comparison object substring associated with max score)
+        """
         sys.setrecursionlimit(10**6)
         rows = self.string
         cols = comparison_ob.string
@@ -153,6 +218,15 @@ class Fasta():
         end_row, end_col = max_loc
 
         def find_roots(row, col):
+            """Recursively find the start of the alignment associated with the maximum alignment score
+
+            Args:
+                row (int): row
+                col (int): column
+
+            Returns:
+                set: set of roots that could be the start of the max alignment
+            """
             roots = set()
             if dist[row][col] == 0:
                 roots.add((row, col))
@@ -165,10 +239,20 @@ class Fasta():
         for (start_row, start_col) in list(starts):
             return max_score, rows[start_row:end_row], cols[start_col:end_col]
 
+        # Should never be reached (alignment not found that matches max score)
         return max_score, "failed"
     
-    # Returns the fitting alignment of two strings. 
     def fitting_alignment(self, motif_ob, mapper, gap_pen):
+        """Finds the maximum alignment score of a motif in the Fasta string
+
+        Args:
+            motif_ob (Fasta): object whose string represents the motif
+            mapper (dict): dict whose keys should be the substitution chars, and values should be the score associated with them
+            gap_pen (int): the penalty for insertions or deletions
+
+        Returns:
+            int: maximum alinement score of the motif 
+        """
         sys.setrecursionlimit(10**6)
         rows = motif_ob.dna_string
         cols = self.dna_string
@@ -207,6 +291,7 @@ class Fasta():
             col_string = cols[start_col:end_col]
 
         ###########################################
+        # modified edit dist alignment 
         cols = col_string
         # (ROW, COL, NUM)
         dist = [[("", "", 0) for _ in range(len(cols)+1)] for _ in range(len(rows)+1)]
